5db868f2bfd24b0d8acc513354a55a4f2feef48a
riyadennis <riyacv@gmail.com>
2020-02-25 16:16:21 +0000

    added file name into the print

--- ch2/comandline/main.go
+++ ch2/comandline/main.go
@@ -7,8 +7,14 @@ import (
 	"strings"
 )
 
+type fileDetails struct {
+	name string
+	counts map [string]int
+}
+
 func main() {
-	counts := make(map[string]int)
+	var fds []*fileDetails
+	counts := make(map [string]int)
 	// everything in command line argument
 	// other than the first one.
 	files := os.Args[1:]
@@ -17,19 +23,27 @@ func main() {
 		return
 	} else {
 		for _, file := range files {
+			fd := &fileDetails{
+				name:   file,
+				counts: counts,
+			}
 			f, err := ioutil.ReadFile(file)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "%v\n", err)
 				continue
 			}
 			for _, l := range strings.Split(string(f), "\n"){
-				counts[l]++
+				fd.counts[l]++
 			}
+			fds = append(fds, fd)
 		}
 	}
-	for line, count := range counts {
-		if line != "" && count > 1 {
-			fmt.Fprintf(os.Stdout, "%s repeated : %d times\n", line, count)
+	for _,  fd := range fds {
+		for line, count := range fd.counts{
+			if line != "" && count > 1 {
+				fmt.Fprintf(os.Stdout, "%s repeated : %d times in file %s\n",
+					line, count, fd.name)
+			}
 		}
 	}
 }
